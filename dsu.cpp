2c633eb833b98624de7f095e69ac20aeb5d33a08
Pavle Janevski <34039667+papa@users.noreply.github.com>
2020-06-03 13:59:01 +0200

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -5,6 +5,11 @@ using namespace std;
 typedef long long ll;
 
 //disjoint set union
+//primene:
+//1) MST - Kruskal
+//2) Broj komponenti u grafu
+//3) Cuvanje odredjenih informacija o nekom setu 
+//4) Painting subarrays offline
 
 struct DSU //sa svim optimizacijama u proseku je slozenost konstanta
 {
@@ -55,6 +60,60 @@ struct DSU //sa svim optimizacijama u proseku je slozenost konstanta
         }
     }
     
+};
+
+struct DSU_with_distance 
+{
+    //ova struktura takodje cuva i udaljenost
+    //svakog cvora od svog predstavnika
+    int n;
+    vector<pair<int,int>> p;
+    vector<int> rank;
+    
+    DSU_with_distance()
+    {
+        
+    }
+    
+    void build()
+    {
+        for(int i=1;i<=n+1;i++) 
+        {
+            p[i] = {i,0};
+            rank[i] = 0;
+        }
+    }
+    
+    DSU_with_distance(int nn)
+    {
+        this->n = nn;
+        p.resize(nn+2);
+        rank.resize(nn+2);
+        build();
+    }
+    
+    pair<int,int> find_set(int v)
+    {
+        if(v!=p[v].first)
+        {
+            int len = p[v].second;
+            p[v] = find_set(p[v].first);
+            p[v].second+=len;
+        }
+        return p[v];
+    }
+    
+    void union_sets(int a,int b)
+    {
+        a = find_set(a).first;
+        b = find_set(b).second;
+        if(a!=b)
+        {
+            if(rank[a] < rank[b]) swap(a,b);
+            p[b] = {a,1};
+            if(rank[a]==rank[b]) rank[a]++;
+        }
+    }
 };
 
 
