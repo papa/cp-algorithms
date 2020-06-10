7e7963e26784a6a645e947a4c31342d8a1953ce3
Pavle Janevski <34039667+papa@users.noreply.github.com>
2020-06-10 21:40:03 +0200

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -80,6 +80,103 @@ struct segment_tree
 };
 
 
+
+//za odredjeni segment [l1,r1] naci segment [l2,r2] l<=l2,r2<=r tako da je suma maksimalna
+struct segment_tree_2
+{
+    struct data
+    {
+        int sum;
+        int pref;
+        int suf;
+        int ans;
+    };
+
+    //cuvamo za svaki segment njegovu sumu 
+    //maksimalni prefiks,maksimalni sufiks
+    //kao i resenje tog segmenta
+    
+    int n;
+    vector<data> seg;
+    vector<int> a;
+    
+    segment_tree_2()
+    {
+        
+    }
+    
+    segment_tree_2(int nn)
+    {
+        this->n = nn;
+        seg.resize(4*n+5);
+    }
+    
+    segment_tree_2(int nn,vector<int> aa)
+    {
+        this->n = nn;
+        seg.resize(4*n+5);
+        a = aa;
+    }
+    
+    data f(data l,data r) //kombinovanje dva segmenta
+    {
+        data res;
+        res.sum = l.sum + r.sum;
+        res.pref = max(l.pref,l.sum + r.pref);
+        res.suf = max(r.suf,r.sum+l.suf);
+        res.ans = max(max(l.ans,r.ans),r.pref + l.suf);
+        return res;
+    }
+    
+    data novi(int x)
+    {
+        data res;
+        res.sum = x;
+        res.pref = res.suf = res.ans = max(0,x);
+        return res;
+    }
+    
+    void build(int id,int tl,int tr)
+    {
+        if(tl==tr)
+        {
+            seg[id] = novi(a[tl]);
+        }
+        else
+        {
+            int tm = (tl+tr)/2;
+            build(2*id,tl,tm);
+            build(2*id+1,tm+1,tr);
+            seg[id]=f(seg[2*id],seg[2*id+1]);
+        }
+    }
+    
+    data query(int id,int tl,int tr,int l,int r)
+    {
+        if(l > r) return novi(0); 
+        if(l==tl && r==tr) return seg[id];
+        int tm = (tl+tr)/2;
+        return f(query(2*id,tl,tm,l,min(r,tm)),
+                 query(2*id+1,tm+1,tr,max(l,tm+1),r));
+    }
+    
+    void update(int id,int tl,int tr,int p,int v)
+    {
+        if(tl==tr)
+        {
+            seg[id] = novi(v);
+        }
+        else
+        {
+            int tm = (tl+tr)/2;
+            if(p<=tm) update(id*2,tl,tm,p,v);
+            else update(2*id+1,tm+1,tr,p,v);
+            seg[id] = f(seg[2*id],seg[2*id+1]);
+        }
+    }
+};
+
+
 int main() 
 {
     ios_base::sync_with_stdio(false);
